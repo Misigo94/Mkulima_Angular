645cbd4bfad1acb0ad1180c6b78913187c5351f2
Misigomartin <Misigomartin@gmail.com>
2022-06-30 10:38:42 +0300

    created angular application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AppComponent } from './app.component';
+import { CatalogueComponent } from './catalogue/catalogue.component';
+import { ContactComponent } from './contact/contact.component';
+import { FeedsComponent } from './feeds/feeds.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'feeds', component: FeedsComponent},
+  { path: 'contact', component: ContactComponent },
+  { path: 'catalogue', component: CatalogueComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { CatalogueComponent } from './catalogue/catalogue.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { FeedsComponent } from './feeds/feeds.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    CatalogueComponent,
+    ContactComponent,
+    FooterComponent,
+    FeedsComponent
   ],
   imports: [
     BrowserModule,
