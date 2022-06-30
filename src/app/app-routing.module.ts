810ebc246c0d8af193aac7b556170fbf9de13a04
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-06-30 14:30:24 +0300

    completed the landing page of the application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { CatalogueComponent } from './catalogue/catalogue.component';
 import { ContactComponent } from './contact/contact.component';
 import { FeedsComponent } from './feeds/feeds.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent },
+  { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'feeds', component: FeedsComponent},
   { path: 'contact', component: ContactComponent },
